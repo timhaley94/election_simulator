90e0a4cc22faf5c9ec9bdfb56fc25491db03e1da
Tim Haley <tim.haley@getbellhops.com>
2019-02-17 13:56:59 -0500

    Almost done with models. Need to do history modules, and runElection function. Also need to write tests.

---
+++ src/models/history.js
@@ -0,0 +1,7 @@
+export function makeHistory() {
+
+}
+
+export function extendHistory() {
+
+}
--- src/models/simulation.js
+++ src/models/simulation.js
@@ -1,34 +1,22 @@
-const VOTERS_IN_DISTRICT = 100;
-const DISTRICTS_IN_ELECTORATE = 50;
+import { makeHistory, extendHistory } from './history';
+import { makeElectorate, runElection } from './electorate';
 
-function makeDistrict(parties) {
-  return repeat(
-    () => makeVoter(parties),
-    VOTERS_IN_DISTRICT
-  );
+export function create() {
+  const history = makeHistory();
+  const electorate = makeElectorate();
+
+  return {
+    history,
+    electorate
+  };
 }
 
-export function makeElectorate() {
-  const parties = makeParties();
+export function step({ electorate, history }) {
+  const results = runElection(electorate, history);
+  history = extendHistory(history, results);
 
-  return repeat(
-    () => makeDistrict(parties),
-    DISTRICTS_IN_ELECTORATE
-  );
-}
-
-function runDistrictElection(district) {
-  const votes = parties.keySeq();
-
-  district.forEach(
-    voter => {
-      const selectedPartyId = selectVote(voter, history);
-
-
-    }
-  );
-}
-
-export function runElection() {
-
+  return {
+    history,
+    electorate
+  };
 }
